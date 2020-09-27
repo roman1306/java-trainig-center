9ecc52b09298f2d19e9ffced604d81195439d4cf
Roman Petrenko <rpetrenko51@gmail.com>
2020-09-27 19:54:28 +0400

    initial commit

---
+++ src/main/java/com/github/roman1306/center/Main.java
@@ -0,0 +1,10 @@
+package com.github.roman1306.center;
+
+import com.github.roman1306.center.repository.sort.TypeSortStudent;
+import com.github.roman1306.center.service.ServiceStudent;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
